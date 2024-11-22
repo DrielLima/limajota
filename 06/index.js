b03faf19e23447a844a423f371d4b376093c07af
DrielLima <160731830+DrielLima@users.noreply.github.com>
2024-11-22 10:18:30 -0300

    Add files via upload

---
+++ 06/index.js
@@ -0,0 +1,21 @@
+function raiz25(){ 
+    var Raiz = Math.sqrt(25) 
+    alert("A raiz de 25 é "+ Raiz) 
+} 
+function raiz36(){
+    var Raiz = Math.sqrt(36)
+    alert("A raiz de 36 é "+ Raiz) 
+} 
+function raiz49(){
+    var Raiz = Math.sqrt(49) 
+    console.log(Raiz) 
+    alert("A raiz de 49 é "+ Raiz)
+}   
+function escreveDiv(texto){
+    alert(texto) 
+    document.getElementById('mydiv').innerHTML = texto 
+}
+function escreveDiv2(){
+    var texto2 = prompt('XAROPINHO: ') 
+    document.getElementById('scriptdiv').innerHTML = texto2
+}
