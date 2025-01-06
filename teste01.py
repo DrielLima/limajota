6f8704d8d2f074e6593fb17cdbd8161c307accdf
DrielLima <160731830+DrielLima@users.noreply.github.com>
2025-01-06 11:33:44 -0300

    Add files via upload

---
+++ teste01.py
@@ -0,0 +1,21 @@
+print ("Oii SOM,AA, 1, 2, testando, testando som.")
+
+nome = "Adriel" 
+idade= 9 
+altura=1.50
+esta_estudando = True 
+print(nome, idade, altura, esta_estudando)
+
+nome = input("Digite seu nome: ") 
+print("Olá,", nome, "seja bem-vindo!")
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: ")) 
+soma = num1 + num2 
+print("A soma é:", soma)
+
+numero = int(input("Digite um número: "))
+if numero % 2 == 0: 
+    print("O número é par.")
+else: 
+    print("O número é ímpar.")
